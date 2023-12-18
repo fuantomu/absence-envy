9c76a3da21b6e1c2c02db53fdbb405bdd1bb95cc
fuantomu <fuantomuw@gmail.com>
2023-12-18 08:57:33 +0100

    fixed api url

--- src/utils/RosterProvider/index.ts
+++ src/utils/RosterProvider/index.ts
@@ -3,13 +3,13 @@ import { BuildPlayer } from "../../types";
 export abstract class RosterProvider {
 
   public static async getRosterRaidPlayers(connectionString: string) : Promise<BuildPlayer[]>{
-    return await fetch(`http://${process.env.REACT_APP_BASEURL}:8080/build/sql/import`, {method: "POST", mode:"cors",credentials:"include", headers: {"Content-Type": "application/json"}, body: connectionString}).then((response) => response.json()).then((roster) => {
+    return await fetch(`http://${process.env.REACT_APP_BASEURL}:8080/builds/roster/import`, {method: "POST", mode:"cors",credentials:"include", headers: {"Content-Type": "application/json"}, body: connectionString}).then((response) => response.json()).then((roster) => {
       return roster.players
     })
   }
 
   public static async sendAbsence(connectionString: string) : Promise<BuildPlayer[]>{
-    return await fetch(`http://${process.env.REACT_APP_BASEURL}:8080/build/sql/absence/save`, {method: "POST", mode:"cors",credentials:"include", headers: {"Content-Type": "application/json"}, body: connectionString}).then((response) => response.json()).then((roster) => {
+    return await fetch(`http://${process.env.REACT_APP_BASEURL}:8080/builds/absence/save`, {method: "POST", mode:"cors",credentials:"include", headers: {"Content-Type": "application/json"}, body: connectionString}).then((response) => response.json()).then((roster) => {
       return roster
     })
   }
