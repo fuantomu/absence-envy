4cb8b60e77061a24f529257eae0f9062a1bfa7ed
fuantomu <fuantomuw@gmail.com>
2023-12-29 13:11:47 +0100

    updated api

--- src/utils/RosterProvider/index.ts
+++ src/utils/RosterProvider/index.ts
@@ -3,13 +3,13 @@ import { BuildPlayer } from "../../types";
 export abstract class RosterProvider {
 
   public static async getPlayers() : Promise<BuildPlayer[]>{
-    return await fetch(`http://${process.env.REACT_APP_BASEURL}:8080/player/`, {method: "GET", mode:"cors",credentials:"include"}).then((response) => response.json()).then((roster) => {
+    return await fetch(`${process.env.REACT_APP_API}/player/`, {method: "GET", mode:"cors",credentials:"include"}).then((response) => response.json()).then((roster) => {
       return roster
     })
   }
 
   public static async sendAbsence(absence: string){
-    await fetch(`http://${process.env.REACT_APP_BASEURL}:8080/absence/`, {method: "POST", mode:"cors",credentials:"include", headers: {"Content-Type": "application/json"}, body: absence}).then((response) =>{
+    await fetch(`${process.env.REACT_APP_API}/absence/`, {method: "POST", mode:"cors",credentials:"include", headers: {"Content-Type": "application/json"}, body: absence}).then((response) =>{
       console.log(response)
     })
   }
