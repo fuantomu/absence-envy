92f0c3828b8ff13ae31160f9fbf4a56fea101471
fuantomu <fuantomuw@gmail.com>
2024-01-26 19:15:30 +0100

    fixed absence message

--- src/pages/AbsencePage/index.tsx
+++ src/pages/AbsencePage/index.tsx
@@ -7,14 +7,22 @@ import Loading from "../../components/Loading";
 import { BuildHelper } from "../../utils/BuildHelper";
 import useErrorHandler from "../../utils/useErrorHandler";
 import { BuildPlayer } from "../../types";
-import { Box, Button, MenuItem, Select, SelectChangeEvent, Tooltip, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import UUID from "../../utils/UUID";
-import SendIcon from '@mui/icons-material/Send';
+import SendIcon from "@mui/icons-material/Send";
 import Logo from "../Logo";
 import useStyles from "./useStyles";
 import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import dayjs, { Dayjs } from 'dayjs';
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
 import ReasonText from "../ReasonText";
 import useWebSocket from "react-use-websocket";
 
@@ -26,99 +34,128 @@ const AbsencePage: FC<AbsencePageProps> = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [selectedOption, setSelectedOption] = useState("DEFAULT");
   const [options, setOptions] = useState<any[]>([]);
-  const [startDate, setStartDate] = useState<Dayjs>(dayjs().set("hours",0).set("minutes", 0).set("seconds", 0).set("milliseconds",0));
-  const [endDate, setEndDate] = useState<Dayjs>(dayjs().set("hours",0).set("minutes", 0).set("seconds", 0).set("milliseconds",0));
+  const [startDate, setStartDate] = useState<Dayjs>(
+    dayjs().set("hours", 0).set("minutes", 0).set("seconds", 0).set("milliseconds", 0)
+  );
+  const [endDate, setEndDate] = useState<Dayjs>(
+    dayjs().set("hours", 0).set("minutes", 0).set("seconds", 0).set("milliseconds", 0)
+  );
   const [sent, setSent] = useState(false);
   const [players, setPlayers] = useState<BuildPlayer[]>([]);
   const [characterError, setCharacterError] = useState(false);
-  const [socketUrl,] = useState(process.env.REACT_APP_WEBSOCKET);
+  const [socketUrl] = useState(process.env.REACT_APP_WEBSOCKET);
   const handleError = useErrorHandler();
-  const reasonRef = useRef<any>()
+  const reasonRef = useRef<any>();
 
-  const { sendMessage } = useWebSocket(socketUrl??"", { shouldReconnect: (closeEvent) => true,
-                                                        reconnectAttempts: 10,
-                                                        reconnectInterval: (attemptNumber) =>
-                                                        Math.min(Math.pow(2, attemptNumber) * 1000, 10000)
-                                                      })
+  const { sendMessage } = useWebSocket(socketUrl ?? "", {
+    shouldReconnect: (closeEvent) => true,
+    reconnectAttempts: 10,
+    reconnectInterval: (attemptNumber) => Math.min(Math.pow(2, attemptNumber) * 1000, 10000),
+  });
 
   const getOptions = async () => {
-    const buildObject : any[] = [];
+    const buildObject: any[] = [];
     const newPlayers: BuildPlayer[] = [];
-    await BuildHelper.parseGetPlayers().then((newRoster : BuildPlayer[]) => {
-      if(newRoster){
-        for(const player of newRoster){
-          if(player.main === player.name){
-            newPlayers.push(player)
-            buildObject.push(player.name)
+    await BuildHelper.parseGetPlayers().then((newRoster: BuildPlayer[]) => {
+      if (newRoster) {
+        for (const player of newRoster) {
+          if (player.main === player.name) {
+            newPlayers.push(player);
+            buildObject.push(player.name);
           }
         }
-        setOptions(buildObject.sort((a,b) => a.localeCompare(b)))
+        setOptions(buildObject.sort((a, b) => a.localeCompare(b)));
       }
-    })
-    setPlayers([...newPlayers])
-    return buildObject
-  }
+    });
+    setPlayers([...newPlayers]);
+    return buildObject;
+  };
 
   const handleSelect = (event: SelectChangeEvent<string>, child: ReactNode) => {
-    setSelectedOption(event.target.value)
-    setCharacterError(false)
-  }
+    setSelectedOption(event.target.value);
+    setCharacterError(false);
+  };
 
   const handleSend = () => {
-    if(selectedOption === "DEFAULT"){
-      setCharacterError(true)
-      return
+    if (selectedOption === "DEFAULT") {
+      setCharacterError(true);
+      return;
     }
-    if(!reasonRef.current?.value){
-      return
-    }
-
-    const newStart = startDate.set("hours",0).set("minutes",0).set("seconds",0).set("milliseconds",0)
-    const newEnd = endDate.set("hours",23).set("minutes",59).set("seconds",59).set("milliseconds",999)
-    console.log(`Player ${selectedOption} is absent from ${newStart} to ${newEnd} with reason ${reasonRef.current?.value?? ""}`)
-    const foundPlayer = players.find((player) => player.name === selectedOption)
-    if(foundPlayer){
-      const absence = {player_id:foundPlayer.id, start_date: newStart.unix()*1000, end_date: newEnd.unix()*1000, reason: reasonRef.current?.value?? ""}
-      BuildHelper.parseAbsenceSend(absence)
-      BuildHelper.parsePostAbsence(absence, selectedOption)
-      sendMessage(JSON.stringify({messageType:"absence", data:absence}))
-      setSent(true)
+    if (!reasonRef.current?.value) {
+      return;
     }
 
-  }
+    const newStart = startDate
+      .set("hours", 0)
+      .set("minutes", 0)
+      .set("seconds", 0)
+      .set("milliseconds", 0);
+    const newEnd = endDate
+      .set("hours", 23)
+      .set("minutes", 59)
+      .set("seconds", 59)
+      .set("milliseconds", 999);
+    console.log(
+      `Player ${selectedOption} is absent from ${newStart} to ${newEnd} with reason ${
+        reasonRef.current?.value ?? ""
+      }`
+    );
+    const foundPlayer = players.find((player) => player.name === selectedOption);
+    if (foundPlayer) {
+      const absence = {
+        player_id: foundPlayer.id,
+        start_date: newStart.unix() * 1000,
+        end_date: newEnd.unix() * 1000,
+        reason: reasonRef.current?.value ?? "",
+      };
+      BuildHelper.parseAbsenceSend(absence);
+      BuildHelper.parsePostAbsence(absence, selectedOption);
+      sendMessage(
+        JSON.stringify({
+          message_type: "absence",
+          data: absence,
+          date: new Date().getTime(),
+          account_name: "System",
+        })
+      );
+      setSent(true);
+    }
+  };
 
   const handleGoBack = () => {
-    setSelectedOption("DEFAULT")
-    setStartDate(dayjs().set("hours",0).set("minutes", 0).set("seconds", 0).set("milliseconds",0))
-    setEndDate(dayjs().set("hours",0).set("minutes", 0).set("seconds", 0).set("milliseconds",0))
-    setSent(false)
-  }
+    setSelectedOption("DEFAULT");
+    setStartDate(
+      dayjs().set("hours", 0).set("minutes", 0).set("seconds", 0).set("milliseconds", 0)
+    );
+    setEndDate(dayjs().set("hours", 0).set("minutes", 0).set("seconds", 0).set("milliseconds", 0));
+    setSent(false);
+  };
 
   const handleStartDateChange = (date: Dayjs | null) => {
-    if(date === null){
-      date = dayjs().set("hours",0).set("minutes", 0).set("seconds", 0).set("milliseconds",0)
+    if (date === null) {
+      date = dayjs().set("hours", 0).set("minutes", 0).set("seconds", 0).set("milliseconds", 0);
     }
-    const newDate = date.set("hours",0).set("minutes", 0).set("seconds", 0).set("milliseconds",0)
-    setStartDate(newDate)
-    if(newDate.isAfter(endDate)){
-      setEndDate(newDate)
+    const newDate = date.set("hours", 0).set("minutes", 0).set("seconds", 0).set("milliseconds", 0);
+    setStartDate(newDate);
+    if (newDate.isAfter(endDate)) {
+      setEndDate(newDate);
     }
-  }
+  };
 
   const handleEndDateChange = (date: Dayjs | null) => {
-    if(date === null){
-      date = dayjs().set("hours",0).set("minutes", 0).set("seconds", 0).set("milliseconds",0)
+    if (date === null) {
+      date = dayjs().set("hours", 0).set("minutes", 0).set("seconds", 0).set("milliseconds", 0);
     }
-    const newDate = date.set("hours",0).set("minutes", 0).set("seconds", 0).set("milliseconds",0)
-    setEndDate(newDate)
-    if(newDate.isBefore(startDate)){
-      setStartDate(newDate)
+    const newDate = date.set("hours", 0).set("minutes", 0).set("seconds", 0).set("milliseconds", 0);
+    setEndDate(newDate);
+    if (newDate.isBefore(startDate)) {
+      setStartDate(newDate);
     }
-  }
+  };
 
   useEffect(() => {
-    if (isLoading){
-      getOptions()
+    if (isLoading) {
+      getOptions();
       setIsLoading(false);
     }
   }, [handleError, isLoading, options]);
@@ -127,22 +164,28 @@ const AbsencePage: FC<AbsencePageProps> = () => {
     return <Loading />;
   }
 
-  if (sent){
-    return (<Container key={UUID()}>
-              <Box key={UUID()} display={"flex"} css={{justifyContent:"center", marginTop: "10%"}}>
-                <Typography style={{caretColor: "transparent"}} fontSize={"38px"} variant="subtitle1">
-                {common(`absence.sent`)}
-                </Typography>
-              </Box>
-              <Box display={"flex"} css={{marginTop: "80%", justifyContent:"center"}}>
-                <Button color="primary" variant="contained" css={{width:"300px", height:"120px"}} onClick={handleGoBack}>
-                  <Typography style={{caretColor: "transparent"}} fontSize={"38px"} variant="subtitle1">
-                   {"Back"}
-                  </Typography>
-                </Button>
-              </Box>
-            </Container>
-    )
+  if (sent) {
+    return (
+      <Container key={UUID()}>
+        <Box key={UUID()} display={"flex"} css={{ justifyContent: "center", marginTop: "10%" }}>
+          <Typography style={{ caretColor: "transparent" }} fontSize={"38px"} variant="subtitle1">
+            {common(`absence.sent`)}
+          </Typography>
+        </Box>
+        <Box display={"flex"} css={{ marginTop: "80%", justifyContent: "center" }}>
+          <Button
+            color="primary"
+            variant="contained"
+            css={{ width: "300px", height: "120px" }}
+            onClick={handleGoBack}
+          >
+            <Typography style={{ caretColor: "transparent" }} fontSize={"38px"} variant="subtitle1">
+              {"Back"}
+            </Typography>
+          </Button>
+        </Box>
+      </Container>
+    );
   }
 
   return (
@@ -153,28 +196,49 @@ const AbsencePage: FC<AbsencePageProps> = () => {
           <h2>{common("absence.title")}</h2>
           <Box css={styles.content}>
             <Box display={"grid"} width={"100%"}>
-              <Select error={characterError} required css={{minWidth:"200px"}} value={selectedOption} label="Character" onChange={handleSelect} MenuProps={{ PaperProps: { sx: { maxHeight: 300 } } }}>
-                    <MenuItem disabled key={"DEFAULT"} value={"DEFAULT"}>Select a character...</MenuItem>
-                    {options.map((option) => (
-                        <MenuItem key={UUID()} value={option}> {option} </MenuItem>
-                    ))}
+              <Select
+                error={characterError}
+                required
+                css={{ minWidth: "200px" }}
+                value={selectedOption}
+                label="Character"
+                onChange={handleSelect}
+                MenuProps={{ PaperProps: { sx: { maxHeight: 300 } } }}
+              >
+                <MenuItem disabled key={"DEFAULT"} value={"DEFAULT"}>
+                  Select a character...
+                </MenuItem>
+                {options.map((option) => (
+                  <MenuItem key={UUID()} value={option}>
+                    {" "}
+                    {option}{" "}
+                  </MenuItem>
+                ))}
               </Select>
-              {characterError? <Typography style={{caretColor: "transparent", color: "red"}} fontSize={"14px"} variant="subtitle1">
-                {common(`absence.error`)}
-              </Typography> : <></>}
+              {characterError ? (
+                <Typography
+                  style={{ caretColor: "transparent", color: "red" }}
+                  fontSize={"14px"}
+                  variant="subtitle1"
+                >
+                  {common(`absence.error`)}
+                </Typography>
+              ) : (
+                <></>
+              )}
             </Box>
             <Box display={"grid"} width={"100%"}>
               <br></br>
               <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DateTimePicker
-                  css={{zIndex: 0, background: "#1d1d1d"}}
+                  css={{ zIndex: 0, background: "#1d1d1d" }}
                   ampm={false}
                   format="DD.MM.YYYY"
                   label={common("absence.start")}
                   value={startDate}
                   onChange={handleStartDateChange}
-                  views={['year','month','day']}
-                  sx={{minWidth:"200px"}}
+                  views={["year", "month", "day"]}
+                  sx={{ minWidth: "200px" }}
                 />
               </LocalizationProvider>
             </Box>
@@ -182,20 +246,26 @@ const AbsencePage: FC<AbsencePageProps> = () => {
               <br></br>
               <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DateTimePicker
-                  css={{zIndex: 0, background: "#1d1d1d"}}
+                  css={{ zIndex: 0, background: "#1d1d1d" }}
                   ampm={false}
                   format="DD.MM.YYYY"
                   label={common("absence.end")}
                   value={endDate}
                   onChange={handleEndDateChange}
-                  views={['year','month','day']}
-                  sx={{minWidth:"200px"}}
+                  views={["year", "month", "day"]}
+                  sx={{ minWidth: "200px" }}
                 />
               </LocalizationProvider>
             </Box>
             <ReasonText reasonRef={reasonRef}></ReasonText>
             <Tooltip title={common("absence.send")} placement="top" arrow>
-              <Button color="success" variant="contained" size="large" style={{height: '50px', width: '150px'}} onClick={() => handleSend()}>
+              <Button
+                color="success"
+                variant="contained"
+                size="large"
+                style={{ height: "50px", width: "150px" }}
+                onClick={() => handleSend()}
+              >
                 <SendIcon />
               </Button>
             </Tooltip>
